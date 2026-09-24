8ba1811f3c54d1cf6f4f5fe8a8da668a8701e48f
Sharvi Kalaskar <sharvikalaskar268@gmail.com>
2026-09-24 18:11:24 +0530

    Add files via upload

---
+++ date.cpp
@@ -0,0 +1,37 @@
+#include<iostrem>
+using namespace std;
+{
+    class A[2][2];
+    public: matrixBase();
+    void in(), out();
+
+};
+matrixBase::matricBase ()
+{
+    int i, j;
+    for( i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            A[i][j] = 0;
+        }
+    }
+}
+void matrixBase :: in()
+{
+    int i, j;
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            cout<<"Enter number: ";
+            cin>> A[i][j];
+        }
+    }
+}
+void matrixBase:: out()
+{
+    int i, j;
+    for(i = 0; i < 2; )
+}
+
